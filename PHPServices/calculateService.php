e1dd21ec1f3cb29572e57382df69843a8c7ba548
Liipe <luiz-dasilva@hotmail.com>
2015-11-02 21:23:57 -0200

    calculateService revised

--- PHPServices/calculateService.php
+++ PHPServices/calculateService.php
@@ -15,24 +15,40 @@ class calculateService{
         }
     }
     
-    public static function calculateIRPF($salary){
-        
-            if($salary >= 1903.99 && $salary <= 2826.65){
-                return ($salary * 0.75) - 142.80;
-            } 
-             elseif($salary >= 2826.66 && $salary <= 3751.05){
-                return($salary * 0.15) - 354.80;
-            }
-             elseif($salary >= 3751.06 && $salary <= 4664.68){
-                return($salary * 0.225) - 636.13;
-            }
-             elseif($salary > 4664.68){
-                return ($salary * 0.275) - 869.38;
-        }    
+    public static function calculateBaseSalary($salary){
+        $inss = self::calculateINSS($salary);
+        return $salary - $inss;
     }
     
-    public static function calculateSalary($salary){
-        //...
+    public static function calculateIRPF($baseSalary){
+    
+        $irpf = 0.00;    
+    
+        if($baseSalary >= 1903.99 && $baseSalary <= 2826.65){
+             $irpf = ($baseSalary * 0.075) - 142.80;
+        } 
+         elseif($baseSalary >= 2826.66 && $baseSalary <= 3751.05){
+             $irpf = ($baseSalary * 0.15) - 354.80;
+        }
+         elseif($baseSalary >= 3751.06 && $baseSalary <= 4664.68){
+             $irpf = ($baseSalary * 0.225) - 636.13;
+        }
+         elseif($baseSalary > 4664.68){
+             $irpf = ($baseSalary * 0.275) - 869.36;
+        }
+        
+        return  $irpf;
+    }
+    
+    public static function calculateLiquidSalary($salary){
+        $liquidSalary = 0.0;
+        
+        $inss = self::calculateINSS($salary);
+        $baseSalary = self::calculateBaseSalary($salary);
+        $irpf = self::calculateIRPF($salary - $inss);
+        $liquidSalary = $salary - $inss - $irpf;
+        
+        return $liquidSalary;
     }
 }
 
